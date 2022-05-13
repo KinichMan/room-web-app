d21dcae511ba034859655975d66e20b427d0c280
KinichMan <kinichman@gmail.com>
2022-05-13 17:32:12 -0500

    Implementing Spring Data

--- src/main/java/com/uma/roomwebapp/Room.java
+++ src/main/java/com/uma/roomwebapp/Room.java
@@ -5,18 +5,35 @@
  */
 package com.uma.roomwebapp;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Kinich Mansilla
  */
-public class Room {
+@Entity
+@Table(name = "ROOM")
+public class Room{
 
+
+    @Id
+    @Column(name = "ROOM_ID")
+    @GeneratedValue
     private long id;
+    @Column(name = "NAME")
     private String name;
+    @Column(name = "ROOM_NUMBER")
     private String number;
+    @Column(name = "BED_INFO")
     private String info;
 
     public Room() {
+        super();
     }
 
     public Room(long id, String name, String number, String info) {
--- src/main/java/com/uma/roomwebapp/RoomServices.java
+++ src/main/java/com/uma/roomwebapp/RoomServices.java
@@ -7,6 +7,7 @@ package com.uma.roomwebapp;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,15 +17,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class RoomServices {
 
-    private static List<Room> rooms = new ArrayList<>();
+    private RoomRepository roomRepository;
 
-    static {
-        for (int i = 0; i < 10; i++) {
-            rooms.add(new Room(i, "Room " + i, "R" + i, "Q"));
-        }
+    @Autowired
+    public RoomServices(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
     }
 
-    public List<Room> getAllRooms(){
+    public List<Room> getAllRooms() {
+        List<Room> rooms = new ArrayList<>();
+        this.roomRepository.findAll().forEach(rooms::add);
         return rooms;
     }
+
 }
